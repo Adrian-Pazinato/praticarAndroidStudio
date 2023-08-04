cf820d95f736b8c1eceea268cea143b9cd4f4a3f
Adrian Pazinato <82603081+Adrian-Pazinato@users.noreply.github.com>
2023-08-04 14:03:27 -0300

    Foda

--- PokemonApp/app/src/main/java/com/example/pokemonmanagerAdix/MainActivity.java
+++ PokemonApp/app/src/main/java/com/example/pokemonmanagerAdix/MainActivity.java
@@ -10,5 +10,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPokemon1 = findViewById(R.id.btnPokemon1);
+        Button btnPokemon2 = findViewById(R.id.btnPokemon2);
+
+        btnPokemon1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Código para abrir a nova atividade quando o botão Pokemon 1 for clicado
+                Intent intent = new Intent(MainActivity.this, Pokemon1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPokemon2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Código para abrir a nova atividade quando o botão Pokemon 2 for clicado
+                Intent intent = new Intent(MainActivity.this, Pokemon2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
