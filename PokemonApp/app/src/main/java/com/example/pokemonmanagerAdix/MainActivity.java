a348859e6d113c1c55dc6abfed9f1b35beb392a6
Adrian Pazinato <82603081+Adrian-Pazinato@users.noreply.github.com>
2023-08-07 14:40:53 -0300

    Renew

--- PokemonApp/app/src/main/java/com/example/pokemonmanagerAdix/MainActivity.java
+++ PokemonApp/app/src/main/java/com/example/pokemonmanagerAdix/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.Button;
+
+import android.view.View;
+
+import android.content.Intent;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,22 +20,5 @@ public class MainActivity extends AppCompatActivity {
         Button btnPokemon1 = findViewById(R.id.btnPokemon1);
         Button btnPokemon2 = findViewById(R.id.btnPokemon2);
 
-        btnPokemon1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Código para abrir a nova atividade quando o botão Pokemon 1 for clicado
-                Intent intent = new Intent(MainActivity.this, Pokemon1Activity.class);
-                startActivity(intent);
-            }
-        });
-
-        btnPokemon2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Código para abrir a nova atividade quando o botão Pokemon 2 for clicado
-                Intent intent = new Intent(MainActivity.this, Pokemon2Activity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
