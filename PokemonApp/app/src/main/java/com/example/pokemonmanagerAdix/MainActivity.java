116d2fb0bd0beeb897a940d7edda196124546ae9
Adrian Pazinato <82603081+Adrian-Pazinato@users.noreply.github.com>
2023-08-07 14:59:11 -0300

    Paginas

--- PokemonApp/app/src/main/java/com/example/pokemonmanagerAdix/MainActivity.java
+++ PokemonApp/app/src/main/java/com/example/pokemonmanagerAdix/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.pokemonmanagerAdix;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import android.widget.Button;
-
 import android.view.View;
-
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
         Button btnPokemon1 = findViewById(R.id.btnPokemon1);
         Button btnPokemon2 = findViewById(R.id.btnPokemon2);
 
+        btnPokemon1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Pokemon1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPokemon2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Pokemon2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ PokemonApp/app/src/main/java/com/example/pokemonmanagerAdix/Pokemon2Activity.java
@@ -0,0 +1,14 @@
+package com.example.pokemonmanagerAdix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pokemon2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon2);
+    }
+}
