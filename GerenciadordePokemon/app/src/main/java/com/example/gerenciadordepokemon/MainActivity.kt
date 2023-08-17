34fa738a48130ee9afc24b578b9f2da8319f19ea
Adrian Pazinato <82603081+Adrian-Pazinato@users.noreply.github.com>
2023-08-17 10:10:31 -0300

    Pequenas Mudanças

--- GerenciadordePokemon/app/src/main/java/com/example/gerenciadordepokemon/MainActivity.kt
+++ GerenciadordePokemon/app/src/main/java/com/example/gerenciadordepokemon/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Define o layout da interface do usuário da atividade
         setContentView(R.layout.activity_main)
     }
 }
