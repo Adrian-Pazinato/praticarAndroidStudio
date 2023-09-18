dbff2538900ace80e1abc0f95931ed2e9172e549
Adrian Pazinato <82603081+Adrian-Pazinato@users.noreply.github.com>
2023-09-18 11:27:11 -0300

    Reset

--- MeusPokemon/app/src/main/java/com/example/meuspokemon/MainActivity.kt
+++ MeusPokemon/app/src/main/java/com/example/meuspokemon/MainActivity.kt
@@ -1,30 +1,11 @@
 package com.example.meuspokemon
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-
-        val bt_inventario = findViewById<Button>(R.id.bt_inventario)
-        val bt_pokemon = findViewById<Button>(R.id.bt_pokemon)
-
-
-        bt_inventario.setOnClickListener {
-            startActivity(Intent(this, PagInventario::class.java))
-        }
-
-        bt_pokemon.setOnClickListener {
-            startActivity(Intent(this, PagPokemon::class.java))
-        }
-
-
     }
 }
