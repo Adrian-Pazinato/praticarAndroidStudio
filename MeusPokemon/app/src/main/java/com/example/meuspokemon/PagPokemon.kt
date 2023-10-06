f80267f887f3c9fe13432ce8acb356e85e8f000c
Adrian Pazinato <82603081+Adrian-Pazinato@users.noreply.github.com>
2023-10-06 10:41:00 -0300

    Abrir imagem da galeria - Porem nao salva

--- MeusPokemon/app/src/main/java/com/example/meuspokemon/PagPokemon.kt
+++ MeusPokemon/app/src/main/java/com/example/meuspokemon/PagPokemon.kt
@@ -1,11 +1,31 @@
 package com.example.meuspokemon
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class PagPokemon : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pag_pokemon)
+
+        val btn: Button = findViewById(R.id.add_poke1)
+        btn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == 0) {
+            val uri = data?.data
+            val image : ImageView = findViewById(R.id.imagePoke1)
+            image.setImageURI(uri)
+        }
     }
 }
